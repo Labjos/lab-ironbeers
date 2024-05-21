a06d50a66d130fff5a37a28374ffb00f080282b2
labjos <joseplorentemkt@gmail.com>
2024-05-21 13:04:53 +0200

    iron-beers done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,13 +14,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+app.get('/', (req, res, next) => {
+  res.render('layout');
+});
+
+app.get('/views/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      console.debug(beers);
+      res.render('beers', { beers })
+    });
+    
+});
 
 // ...
 
-// Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Add the route handlers here:
+app.get('/views/random-beer', (req, res) => {
+  punkAPI.getRandom().then(beers => {
+    
+    res.render('random-beers', beers[0]);
+
+    });
+  
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
